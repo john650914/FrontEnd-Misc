3c100cc26752a25a7074a0f4c6df7fbe1f6967e8
John Chang <johnchang@gamania.com>
2023-05-31 17:47:13 +0800

    更新一堆東東

---
+++ 各種測試/vue/00測試/js/comp01.js
@@ -0,0 +1,42 @@
+﻿const comp01 = {
+	template: `
+		<div>
+			<h2>這是comp01.js的HTML</h2>
+			<div>{{myState}}</div>
+			<inner-comp></inner-comp>
+			<button @click="alertt">點我</button>
+		</div>
+	`,
+	data: function(){
+		return{
+			myData: '元件內的data'
+		}
+	},
+	methods: {
+		alertt(){
+			alert(this.myData);
+		}
+	},
+	computed: Vuex.mapState([
+		'myState',
+	])
+	/* computed: {
+		myState(){
+			return this.$store.state.myState;
+		}
+	} */
+};
+
+Vue.component('inner-comp', {
+	template: `
+		<div>
+			<h2>這是inner-comp的HTML</h2>
+			<div>{{myState}}</div>
+		</div>
+	`,
+	computed: Vuex.mapState([
+		'myState',
+	])
+});
+
+export default comp01;
---
+++ 各種測試/vue/00測試/js/default.js
@@ -0,0 +1,15 @@
+﻿import comp01 from './comp01.js';
+
+const store = new Vuex.Store({
+	state: {
+		myState: '這是Vuex的某個state'
+	}
+})
+
+export const vm = new Vue({
+	el: '#app',
+	store,
+	components: {
+		comp01
+	}
+});
